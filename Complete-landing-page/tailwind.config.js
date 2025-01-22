26759d43fa4890d619a4debb2b3881cadbd728dd
TechTrooperAayush <aayushjash78@gmail.com>
2025-01-22 19:25:25 +0530

    made Footer and Gallery Section responsive

--- Complete-landing-page/tailwind.config.js
+++ Complete-landing-page/tailwind.config.js
@@ -10,6 +10,19 @@ module.exports = {
         dmsans: ['DM Sans', 'sans-serif'], 
         roboto: ['Roboto', 'sans-serif'],
       },
+      gridTemplateColumns: {
+      xl: ['412px 1px 294px 1px 412px'],
+       lg : ['412px 1px 294px 1px'],
+       md : ['390px 1px 294px'],
+       sm : ['330px 274px'],
+       mobdevice : ['360px']
+      },
+      gridTemplateRows: {
+       lg : ['463px 204px'],
+       md : ['463px 463px'],
+        sm : ['390px 330px 230px'],
+     mobdevice :  ['390px 330px 250px']
+      }
     },
   },
   plugins: [],
