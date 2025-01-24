1d56db9d9ef9b3ae9686348915feb4d486b2e785
TechTrooperAayush <aayushjash78@gmail.com>
2025-01-24 16:34:02 +0530

    added toggle functionality

--- Complete-landing-page/src/script.js
+++ Complete-landing-page/src/script.js
@@ -1,7 +1,28 @@
-let navbar = document.getElementById('header');
+const navbar = document.getElementById('header');
 let prevpos = window.pageYOffset;
+const openBtn = document.getElementById('open-menu-btn');
+const popMenu = document.getElementById('pop-up-menu');
+const largerdevies = window.matchMedia('min-width:768px' && 'max-width:1024px');
+let navstatus = false;
 
-window.onscroll = function(){
+window.onresize = function(){
+    if(navbar.clientWidth >= 1024)
+        {
+            popMenu.style.display = "none";
+            navbar.style.height = "100px";
+        } 
+    else if(navbar.clientWidth <= 1024)
+    {
+        navbar.style.height = "80px";
+    }
+    else if (navbar.clientWidth <= 640){
+        navbar.style.height = "70px";
+    }
+        console.log("hello" + navbar.clientWidth);
+}
+
+let view = window.onscroll ;
+ view = function(){
     
     let currentpos = window.pageYOffset;
 
@@ -21,3 +42,20 @@ window.onscroll = function(){
 
     prevpos = currentpos;
 }
+
+openBtn.addEventListener("click",function(){
+
+    if(!navstatus)
+    {
+        navbar.style.height = "480px";
+        popMenu.style.display = "flex";
+        navstatus = true;
+    }
+    else{
+        navbar.style.height = "80px";
+        popMenu.style.display = "none";
+        navstatus = false; 
+    }
+
+})
+
